b451f5b6b6a4c192c364e4c298ce83b46aae0ecb
lxiaopei <lxiaopei@vmware.com>
2021-03-09 19:46:21 +0800

    Add ClusterControlPlane apis

--- api_cluster_control_plane.go
+++ api_cluster_control_plane.go
@@ -18,7 +18,7 @@ import (
 	"strings"
 	"fmt"
 
-        "github.com/vmware/go-vmware-nsxt/antreanode"
+        "github.com/vmware/go-vmware-nsxt/clustercontrolplane"
 )
 
 // Linger please
@@ -26,31 +26,32 @@ var (
 	_ context.Context
 )
 
-type AntreaNodesPolicyApiService service
+type SystemAdministrationPolicyClusterControlPlaneApiService service
 
 /*
-AntreaNodesPolicyApiService Delete a Policy Antrea Node
-Removes a specified antrea node. 
+SystemAdministrationPolicyClusterControlPlaneApiService Delete a specified cluster control plane.
+Removes a specified cluster control plane. 
  * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param siteId
  * @param enforcementpointId
- * @param antreaNodeId
+ * @param clusterControlPlaneId
 
 
 */
-func (a *AntreaNodesPolicyApiService) DeletePolicyAntreaNode(ctx context.Context, siteId string, enforcementpointId string, antreaNodeId string) (*http.Response, error) {
+func (a *SystemAdministrationPolicyClusterControlPlaneApiService) DeleteClusterControlPlane(ctx context.Context, siteId string, enforcementpointId string, clusterControlPlaneId string) (*http.Response, error) {
 	var (
 		localVarHttpMethod = strings.ToUpper("Delete")
 		localVarPostBody   interface{}
 		localVarFileName   string
 		localVarFileBytes  []byte
+		
 	)
 
 	// create path and map variables
-	localVarPath := a.client.cfg.BasePath + "/infra/sites/{site-id}/enforcement-points/{enforcementpoint-id}/policy-antrea-nodes/{antrea-node-id}"
+	localVarPath := a.client.cfg.BasePath + "/infra/sites/{site-id}/enforcement-points/{enforcementpoint-id}/cluster-control-planes/{cluster-control-plane-id}"
 	localVarPath = strings.Replace(localVarPath, "{"+"site-id"+"}", fmt.Sprintf("%v", siteId), -1)
 	localVarPath = strings.Replace(localVarPath, "{"+"enforcementpoint-id"+"}", fmt.Sprintf("%v", enforcementpointId), -1)
-	localVarPath = strings.Replace(localVarPath, "{"+"antrea-node-id"+"}", fmt.Sprintf("%v", antreaNodeId), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"cluster-control-plane-id"+"}", fmt.Sprintf("%v", clusterControlPlaneId), -1)
 
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := url.Values{}
@@ -95,6 +96,7 @@ func (a *AntreaNodesPolicyApiService) DeletePolicyAntreaNode(ctx context.Context
 			body: localVarBody,
 			error: localVarHttpResponse.Status,
 		}
+		
 		if localVarHttpResponse.StatusCode == 400 {
 			var v ApiError
 			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
@@ -157,29 +159,29 @@ func (a *AntreaNodesPolicyApiService) DeletePolicyAntreaNode(ctx context.Context
 }
 
 /*
-AntreaNodesPolicyApiService Return Policy Antrea Node Information
-Returns information about a specific policy antrea node. 
+SystemAdministrationPolicyClusterControlPlaneApiService Return a specified cluster control plane.
+Returns a specified cluster control plane. 
  * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param siteId
  * @param enforcementpointId
- * @param antreaNodeId
+ * @param clusterControlPlaneId
 
-@return PolicyAntreaNode
+@return ClusterControlPlane
 */
-func (a *AntreaNodesPolicyApiService) GetPolicyAntreaNode(ctx context.Context, siteId string, enforcementpointId string, antreaNodeId string) (antreanode.PolicyAntreaNode, *http.Response, error) {
+func (a *SystemAdministrationPolicyClusterControlPlaneApiService) GetClusterControlPlane(ctx context.Context, siteId string, enforcementpointId string, clusterControlPlaneId string) (clustercontrolplane.ClusterControlPlane, *http.Response, error) {
 	var (
 		localVarHttpMethod = strings.ToUpper("Get")
 		localVarPostBody   interface{}
 		localVarFileName   string
 		localVarFileBytes  []byte
-		localVarReturnValue antreanode.PolicyAntreaNode
+		localVarReturnValue clustercontrolplane.ClusterControlPlane
 	)
 
 	// create path and map variables
-	localVarPath := a.client.cfg.BasePath + "/infra/sites/{site-id}/enforcement-points/{enforcementpoint-id}/policy-antrea-nodes/{antrea-node-id}"
+	localVarPath := a.client.cfg.BasePath + "/infra/sites/{site-id}/enforcement-points/{enforcementpoint-id}/cluster-control-planes/{cluster-control-plane-id}"
 	localVarPath = strings.Replace(localVarPath, "{"+"site-id"+"}", fmt.Sprintf("%v", siteId), -1)
 	localVarPath = strings.Replace(localVarPath, "{"+"enforcementpoint-id"+"}", fmt.Sprintf("%v", enforcementpointId), -1)
-	localVarPath = strings.Replace(localVarPath, "{"+"antrea-node-id"+"}", fmt.Sprintf("%v", antreaNodeId), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"cluster-control-plane-id"+"}", fmt.Sprintf("%v", clusterControlPlaneId), -1)
 
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := url.Values{}
@@ -231,7 +233,7 @@ func (a *AntreaNodesPolicyApiService) GetPolicyAntreaNode(ctx context.Context, s
 		}
 		
 		if localVarHttpResponse.StatusCode == 200 {
-			var v antreanode.PolicyAntreaNode
+			var v clustercontrolplane.ClusterControlPlane
 			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
 				if err != nil {
 					newErr.error = err.Error()
@@ -303,25 +305,25 @@ func (a *AntreaNodesPolicyApiService) GetPolicyAntreaNode(ctx context.Context, s
 }
 
 /*
-AntreaNodesPolicyApiService Return the List of Policy Antrea Nodes
-Returns information about all Policy antrea nodes. 
+SystemAdministrationPolicyClusterControlPlaneApiService Return the List of cluster control planes.
+Returns information about all cluster control planes. 
  * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param siteId
  * @param enforcementpointId
 
-@return PolicyAntreaNodeListResult
+@return ClusterControlPlaneListResult
 */
-func (a *AntreaNodesPolicyApiService) ListPolicyAntreaNodes(ctx context.Context, siteId string, enforcementpointId string) (antreanode.PolicyAntreaNodeListResult, *http.Response, error) {
+func (a *SystemAdministrationPolicyClusterControlPlaneApiService) ListClusterControlPlane(ctx context.Context, siteId string, enforcementpointId string) (clustercontrolplane.ClusterControlPlaneListResult, *http.Response, error) {
 	var (
 		localVarHttpMethod = strings.ToUpper("Get")
 		localVarPostBody   interface{}
 		localVarFileName   string
 		localVarFileBytes  []byte
-		localVarReturnValue antreanode.PolicyAntreaNodeListResult
+		localVarReturnValue clustercontrolplane.ClusterControlPlaneListResult
 	)
 
 	// create path and map variables
-	localVarPath := a.client.cfg.BasePath + "/infra/sites/{site-id}/enforcement-points/{enforcementpoint-id}/policy-antrea-nodes"
+	localVarPath := a.client.cfg.BasePath + "/infra/sites/{site-id}/enforcement-points/{enforcementpoint-id}/cluster-control-planes"
 	localVarPath = strings.Replace(localVarPath, "{"+"site-id"+"}", fmt.Sprintf("%v", siteId), -1)
 	localVarPath = strings.Replace(localVarPath, "{"+"enforcementpoint-id"+"}", fmt.Sprintf("%v", enforcementpointId), -1)
 
@@ -375,7 +377,7 @@ func (a *AntreaNodesPolicyApiService) ListPolicyAntreaNodes(ctx context.Context,
 		}
 		
 		if localVarHttpResponse.StatusCode == 200 {
-			var v antreanode.PolicyAntreaNodeListResult
+			var v clustercontrolplane.ClusterControlPlaneListResult
 			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
 				if err != nil {
 					newErr.error = err.Error()
@@ -408,153 +410,6 @@ func (a *AntreaNodesPolicyApiService) ListPolicyAntreaNodes(ctx context.Context,
 		}
 		
 		if localVarHttpResponse.StatusCode == 404 {
-			var v ApiError
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
-				return localVarReturnValue, localVarHttpResponse, newErr
-		}
-		
-		if localVarHttpResponse.StatusCode == 412 {
-			var v ApiError
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
-				return localVarReturnValue, localVarHttpResponse, newErr
-		}
-		
-		if localVarHttpResponse.StatusCode == 500 {
-			var v ApiError
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
-				return localVarReturnValue, localVarHttpResponse, newErr
-		}
-		
-		if localVarHttpResponse.StatusCode == 503 {
-			var v ApiError
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
-				return localVarReturnValue, localVarHttpResponse, newErr
-		}
-		
-		return localVarReturnValue, localVarHttpResponse, newErr
-	}
-
-	return localVarReturnValue, localVarHttpResponse, nil
-}
-
-/*
-AntreaNodesPolicyApiService Created Policy Antrea Node
-Antrea node is created with given configurations.
- * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
- * @param siteId
- * @param enforcementpointId
- * @param policyAntreaNodeRegistrationMessage
-
-@return PolicyAntreaNode
-*/
-func (a *AntreaNodesPolicyApiService) RegisterPolicyAntreaNode(ctx context.Context, siteId string, enforcementpointId string, policyAntreaNodeRegistrationMessage antreanode.PolicyAntreaNodeRegistrationMessage) (antreanode.PolicyAntreaNode, *http.Response, error) {
-	var (
-		localVarHttpMethod = strings.ToUpper("Post")
-		localVarPostBody   interface{}
-		localVarFileName   string
-		localVarFileBytes  []byte
-		localVarReturnValue antreanode.PolicyAntreaNode
-	)
-
-	// create path and map variables
-	localVarPath := a.client.cfg.BasePath + "/infra/sites/{site-id}/enforcement-points/{enforcementpoint-id}/policy-antrea-nodes?action=register_node"
-	localVarPath = strings.Replace(localVarPath, "{"+"site-id"+"}", fmt.Sprintf("%v", siteId), -1)
-	localVarPath = strings.Replace(localVarPath, "{"+"enforcementpoint-id"+"}", fmt.Sprintf("%v", enforcementpointId), -1)
-
-	localVarHeaderParams := make(map[string]string)
-	localVarQueryParams := url.Values{}
-	localVarFormParams := url.Values{}
-
-	// to determine the Content-Type header
-	localVarHttpContentTypes := []string{"application/json"}
-
-	// set Content-Type header
-	localVarHttpContentType := selectHeaderContentType(localVarHttpContentTypes)
-	if localVarHttpContentType != "" {
-		localVarHeaderParams["Content-Type"] = localVarHttpContentType
-	}
-
-	// to determine the Accept header
-	localVarHttpHeaderAccepts := []string{"application/json"}
-
-	// set Accept header
-	localVarHttpHeaderAccept := selectHeaderAccept(localVarHttpHeaderAccepts)
-	if localVarHttpHeaderAccept != "" {
-		localVarHeaderParams["Accept"] = localVarHttpHeaderAccept
-	}
-	// body params
-	localVarPostBody = &policyAntreaNodeRegistrationMessage
-	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
-	if err != nil {
-		return localVarReturnValue, nil, err
-	}
-
-	localVarHttpResponse, err := a.client.callAPI(r)
-	if err != nil || localVarHttpResponse == nil {
-		return localVarReturnValue, localVarHttpResponse, err
-	}
-
-	localVarBody, err := ioutil.ReadAll(localVarHttpResponse.Body)
-	localVarHttpResponse.Body.Close()
-	if err != nil {
-		return localVarReturnValue, localVarHttpResponse, err
-	}
-
-	if localVarHttpResponse.StatusCode < 300 {
-		// If we succeed, return the data, otherwise pass on to decode error.
-		err = a.client.decode(&localVarReturnValue, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-		return localVarReturnValue, localVarHttpResponse, err
-	}
-
-	if localVarHttpResponse.StatusCode >= 300 {
-		newErr := GenericSwaggerError{
-			body: localVarBody,
-			error: localVarHttpResponse.Status,
-		}
-		
-		if localVarHttpResponse.StatusCode == 201 {
-			var v antreanode.PolicyAntreaNode
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
-				return localVarReturnValue, localVarHttpResponse, newErr
-		}
-		
-		if localVarHttpResponse.StatusCode == 400 {
-			var v ApiError
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
-				return localVarReturnValue, localVarHttpResponse, newErr
-		}
-		
-		if localVarHttpResponse.StatusCode == 403 {
 			var v ApiError
 			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
 				if err != nil {
